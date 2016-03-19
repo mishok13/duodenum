adea26b9b1cc823b2663081a8c0070821580a021
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2016-03-19 11:05:47 +0100

    Re-initing everything

--- test/duodenum_test.clj
+++ test/duodenum_test.clj
@@ -1,25 +1,29 @@
 (ns duodenum-test
-  (:require [midje.sweet :refer [fact tabular contains]]
+  (:require [midje.sweet :refer [fact tabular contains future-fact]]
             [duodenum :as d]))
 
+(let [parser (d/parser)]
+  (fact
+   "Empty parser does nothing"
+   (d/parse parser []) => {:arguments [] :unparsed [] :options []}
+   (d/parse parser nil) => {:arguments [] :unparsed [] :options []}
+   (d/parse parser ["foo" "bar"]) => {:arguments [] :unparsed ["foo" "bar"] :options []}
+   (d/parse parser ["--opt=foo" "bar"]) => {:arguments [] :unparsed ["--opt=foo" "bar"] :options []})
+  (fact "Empty parser will "))
+
 (fact
- "Empty parser does nothing"
- (d/parse (d/parser) []) => nil
- (d/parse (d/parser) nil) => nil
+ "Sanity checks pass"
  (d/parse nil []) => nil
- (d/parse nil nil) => nil)
+ (d/parse (d/parser) [123]) => nil)
 
-(fact
- "Option creation has plenty of edge cases")
-
-(fact
+(future-fact
  "Simple argument based parsing works correctly"
  (d/parse (d/parser (d/argument "foo")) ["42"]) => (contains {:arguments {"foo" "42"} :options nil :unparsed nil})
  (d/parse (d/parser (d/argument "foo" :count 1)) ["42"]) => (contains {:arguments {"foo" "42"} :options nil :unparsed nil})
  (d/parse (d/parser (d/argument "foo" :count 1)) ["42" "27"]) => (contains {:arguments {"foo" "42"} :unparsed ["27"]})
  (d/parse (d/parser (d/argument "foo" :count 1)) []) => (contains {:arguments {"foo" nil} :unparsed nil :errors [{:argument "foo" :kind :not-enough-arguments}]}))
 
-(fact
+(future-fact
  "Parsing multiple arguments works correctly"
  (d/parse (d/parser (d/argument "foo") (d/argument "bar")) []) => (contains {:arguments {"foo" nil "bar" nil} :errors [{:argument "foo", :kind :not-enough-arguments} {:argument "bar", :kind :not-enough-arguments}]})
  (d/parse (d/parser (d/argument "foo") (d/argument "bar")) ["42"]) => (contains {:arguments {"foo" "42" "bar" nil} :errors [{:argument "bar", :kind :not-enough-arguments}]})
@@ -27,12 +31,12 @@
  (d/parse (d/parser (d/argument "foo") (d/argument "bar")) ["42" "43" "44"]) => (contains {:arguments {"foo" "42" "bar" "43"} :errors nil :unparsed ["44"]})
  (d/parse (d/parser (d/argument "foo") (d/argument "bar" :count 2)) ["42" "43" "44"]) => (contains {:arguments {"foo" "42" "bar" ["43" "44"]} :errors nil :unparsed nil}))
 
-(fact
+(future-fact
  "Parser with single boolean option (flag) works fine"
  (d/parse (d/parser (d/option "foo" :short "-f")) []) => {:options {"foo" false}}
  (d/parse (d/parser (d/option "foo" :short "-f")) ["-f"]) => {:options {"foo" true}})
 
-;; (fact
+;; (future-fact
 ;;  "Simple option parsing works correctly"
 ;;  (d/parse (d/parser (d/option "foo" :short "-f")) []) => {:arguments nil :options {"foo" false}}
 ;;  (d/parse (d/parser (d/option "foo" :short "-f")) ["-f"]) => {:arguments nil :options {"foo" true}}
@@ -45,12 +49,12 @@
 ;;  (d/parse (d/parser (d/option "foo" :short "-f" :kind :accumulator)) ["-fff"]) => {:arguments nil :options {"foo" ["ff"]}})
 
 ;; (tabular
-;;  (fact
+;;  (future-fact
 ;;   "Accumulator options work correctly"
 ;;   (d/parse (d/parser ?option) ?args) => (contains ?result))
 ;;  ?option ?args ?result
 ;;  (d/option "foo" :short "-f") [] nil)
 
-;; (fact
+;; (future-fact
 ;;  "Mixed option and argument parsing works"
 ;;  (d/parse (d/parser (d/option "foo" :short "-f") (d/argument "bar")) []) => {:arguments {"bar" nil} :options {"foo" false} :errors [{:argument "bar" :kind :not-enough-arguments}]})
