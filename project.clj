84a03007da45550cc3f306b8aafd096aacbadf4f
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2016-03-19 11:30:57 +0100

    Yay fresh stuff

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2"]]}})
